b48e750da69c5141653f7d04ed59acc134e99d98
Rachel Elam <relam@Rachels-MBP.attlocal.net>
2018-01-29 12:55:57 -0500

    finished version second time around

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,58 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = []
-var cardOne = cards [0];
-cardsInPlay.push ('cardOne')
-console.log("User flipped queen")
-var cardTwo = cards [1];
-cardsInPlay.push ('cardTwo')
-console.log("User flipped king")
-if (cardsInPlay.length === [2]) {
-  ("cardOne" === "cardTwo");
-} if (true) {
-  alert ("You found a match!");
-} else {
-  alert ("Sorry, try again");
+var cards = [
+{
+  rank: "queen" ,
+  suit: "hearts" ,
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen" ,
+  suit: "diamonds" ,
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king" ,
+  suit: "hearts" ,
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king" ,
+  suit: "diamonds" ,
+  cardImage: "images/king-of-diamonds.png"
 }
+];
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "queen";
+ console.log("User flipped " +cardOne);
+ console.log("User flipped " +cardThree);
+ console.log(cardImage);
+ console.log(suit);
+var cardsInPlay = []
+cardsInPlay.push(cards[cardId].rank);
+var checkForMatch = function () {
+  if (cardsInPlay [0] === cardsInPlay[1]) {
+checkForMatch();
+    console.log("You found a match!");
+} else {
+    console.log("Sorry, try again.");
+};
+var flipCard = function () {
+  this.getAttribute('data-id');
+    console.log("User flipped" + cards[cardId].rank)
+    cardImage.setAttribute(src[cardId].images
+  if (cardsInPlay.length === [2]) {
+    ("cardOne" === "cardTwo");
+} if (true) {
+    alert ("You found a match!");
+} else {
+    alert ("Sorry, try again");
+};
+var createBoard = function () {
+  for (var i = 0; i <createBoard.length; i++) {
+var newImg = document.createElement('img');
+cardElement('data-id')[i].setAttribute('src', "images/back.png");
+cardElement.addEventListener('click', flipCard);
+document.cardElement.appendChild(game-board);
+}
+createBoard();
